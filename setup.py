5657ebf765e750bab88f32c34dfd399024bc31af
ZhangHaotian <zhtpandog@gmail.com>
2017-10-22 17:05:43 -0700

    add necessary files for publish

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Indexed_Heap',
+  packages = ['Indexed_Heap'],
+  version = '1.0',
+  description = 'A binary indexed heap',
+  author = 'Haotian Zhang',
+  author_email = 'zhtpandog@gmail.com',
+  url = 'https://github.com/zhtpandog/Indexed_Heap', 
+  download_url = 'https://github.com/zhtpandog/Indexed_Heap/archive/1.0.tar.gz', 
+  keywords = ['heap', 'binary heap', 'indexed heap', 'indexed binary heap', 'heapify', 'delmin', 'peek', 'sift', 'priority queue', 'dijkstra', 'mst', 'minimum spanning tree', 'logn'], 
+  classifiers = [],
+)
