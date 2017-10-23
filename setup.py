3e8ea8013303157069b029a009ab475b2b1ba78c
ZhangHaotian <zhtpandog@gmail.com>
2017-10-22 17:07:46 -0700

    minor changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Haotian Zhang',
   author_email = 'zhtpandog@gmail.com',
   url = 'https://github.com/zhtpandog/Indexed_Heap', 
-  download_url = 'https://github.com/zhtpandog/Indexed_Heap/archive/1.0.tar.gz', 
+  download_url = 'https://github.com/zhtpandog/Indexed_Heap/archive/v1.0.tar.gz', 
   keywords = ['heap', 'binary heap', 'indexed heap', 'indexed binary heap', 'heapify', 'delmin', 'peek', 'sift', 'priority queue', 'dijkstra', 'mst', 'minimum spanning tree', 'logn'], 
   classifiers = [],
 )
